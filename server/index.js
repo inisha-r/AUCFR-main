4037f62ba3b94ac789d5cba722126bb9299cdc7e
Dharshini R <114658091+DharshiniR04@users.noreply.github.com>
2024-12-17 22:13:49 +0530

    Updated CORS policy

--- server/index.js
+++ server/index.js
@@ -10,6 +10,18 @@ connectDB();
 
 const app=express();
 app.use(express.json());
+app.use(cors({
+    origin: "https://aucfr-main-client.vercel.app", 
+    methods: ["POST", "GET","PATCH","DELETE"],
+    credentials: true
+  }));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://aucfr-main-client.vercel.app"); 
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, OPTIONS,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
 app.use(cors());
 
 
